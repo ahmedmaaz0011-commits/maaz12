c668a56ad746b9835c18cf5f9677888009766081
Maaz Ahmed <ahmedmaaz0011@gmail.com>
2026-08-25 12:06:56 -0700

    Update Point.java

--- 2nd semester week 3/Point.java
+++ 2nd semester week 3/Point.java
@@ -1,40 +1,32 @@
 public class Point {
-    // 1. Instance variables
     private double x, y;
     
-    // 2. Default Constructor
     public Point() {
         x = 0;
         y = 0;
     }
     
-    // 3. Parameterized Constructor
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
     
-    // 4. Copy Constructor
     public Point(Point p) {
         this.x = p.x;
         this.y = p.y;
     }
     
-    // 5. Getter methods
     public double getX() { return x; }
     public double getY() { return y; }
     
-    // 6. Addition of points
     public Point add(Point p) {
         return new Point(this.x + p.x, this.y + p.y);
     }
     
-    // 7. Subtraction of points
     public Point subtract(Point p) {
         return new Point(this.x - p.x, this.y - p.y);
     }
     
-    // 8. toString method
     @Override
     public String toString() {
         return "(" + x + ", " + y + ")";
@@ -42,10 +34,10 @@ public class Point {
 }
  public class testPoint {
     public static void main(String[] args) {
-        // Test Point class
+
         Point a = new Point(1, 2);
         Point b = new Point(4, 6);
-        Point c = new Point(b); // copy constructor
+        Point c = new Point(b); 
         
         System.out.println("Point A: " + a);
         System.out.println("Point B: " + b);
